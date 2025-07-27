2e46e301c5a5b7eeef609df19d3f71212a14c6e1
CaCC-Lab <support@cacc-lab.net>
2025-07-27 11:49:32 +0900

    fix: DataSourceBadgeテストのインポートを修正

--- frontend/src/components/common/__tests__/DataSourceBadge.test.tsx
+++ frontend/src/components/common/__tests__/DataSourceBadge.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { DataSourceBadge } from '../DataSourceBadge';
+import DataSourceBadge from '../DataSourceBadge';
 
 describe('DataSourceBadge', () => {
   describe('基本的な表示', () => {
